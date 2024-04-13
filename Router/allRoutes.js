5b0ee06134ac2a1ebf6afb450f7aed554df26dc6
Loris Pandaru <loris.buchelet@gmail.com>
2024-04-13 11:29:45 +0200

    adding routing system

---
+++ Router/allRoutes.js
@@ -0,0 +1,8 @@
+import Route from "./Route.js";
+
+// Define routes underneath - add nex pages
+export const allRoutes = [
+    new Route("/studi-arcadia/", "Accueil", "pages/home.html"),];
+
+// Display the title
+export const websiteName = "Zoo d'Arcadia";
